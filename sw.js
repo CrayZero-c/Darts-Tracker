8568c27d2189237ebb43f23e9ee35868dbc11642
CrayZero-c <julian160420@gmail.com>
2025-08-19 12:38:32 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', e => {
   e.waitUntil(caches.open('darts-cache').then(cache => {
-    return cache.addAll(['/', '/index.html']);
+    return cache.addAll(['/', '/index.html', '/manifest.json']);
   }));
 });
 self.addEventListener('fetch', e => {
